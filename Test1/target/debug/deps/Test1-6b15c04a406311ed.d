5d7f0674c0fd9467c9104782e73e37e9a7bc8c47
Swonk888 <112666681+Swonk888@users.noreply.github.com>
2024-09-04 16:41:48 -0600

    add rust project

---
+++ Test1/target/debug/deps/Test1-6b15c04a406311ed.d
@@ -0,0 +1,5 @@
+C:\Users\kevin\OneDrive\Desktop\TEC\Principios de Sistemas Operativos\Entregas\HTTP-Server\Test1\target\debug\deps\libTest1-6b15c04a406311ed.rmeta: src/main.rs
+
+C:\Users\kevin\OneDrive\Desktop\TEC\Principios de Sistemas Operativos\Entregas\HTTP-Server\Test1\target\debug\deps\Test1-6b15c04a406311ed.d: src/main.rs
+
+src/main.rs:
---
+++ Test1/target/debug/deps/Test1-84ca5e6bd1ec019c.d
@@ -0,0 +1,5 @@
+C:\Users\kevin\OneDrive\Desktop\TEC\Principios de Sistemas Operativos\Entregas\HTTP-Server\Test1\target\debug\deps\libTest1-84ca5e6bd1ec019c.rmeta: src/main.rs
+
+C:\Users\kevin\OneDrive\Desktop\TEC\Principios de Sistemas Operativos\Entregas\HTTP-Server\Test1\target\debug\deps\Test1-84ca5e6bd1ec019c.d: src/main.rs
+
+src/main.rs:
